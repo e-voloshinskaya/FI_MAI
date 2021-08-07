fe6fa6b647fa819eb5dc1cf411d3b4941bf40970
e-voloshinskaya <evgeniya@LAPTOP-0GMO7GOG.localdomain>
2021-08-07 16:50:55 +0300

    sql query text added + main.c solution & optimization

--- KP6/main.c
+++ KP6/main.c
@@ -6,30 +6,29 @@
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 2) {
+    if (argc != 2) {
         fprintf(stderr, "Exactly one argument is required\n");
         exit(1);
     }
-    
-    if (argv[1][1] == 'w') {
+    if (argv[1][0] == 'w') {
 
-        if (argc != 2) {
-            fprintf(stderr, "Exactly one argument is required\n");
-            exit(1);
-        }
+        // if (argc != 2) {
+        //     fprintf(stderr, "Exactly one argument is required\n");
+        //     exit(1);
+        // }
 
         printf("%s\n", argv[1]);
         write_file();
 
-    } else if (argv[1][1] == 'r') {
+    } else if (argv[1][0] == 'r') {
 
-        if (argc != 2) {
-            fprintf(stderr, "Exactly one argument is required\n");
-            exit(1);
-        }
+        // if (argc != 2) {
+        //     fprintf(stderr, "Exactly one argument is required\n");
+        //     exit(1);
+        // }
         read_file();
 
-    } else if(argv[1][1] == 'p') {
+    } else if(argv[1][0] == 'p') {
 
         count_scholars();
     }
