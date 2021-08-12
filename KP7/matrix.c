e530e6bb8aa65fbdafbb07ca11572c6c81bbfaa8
e-voloshinskaya <evgeniya@LAPTOP-0GMO7GOG.localdomain>
2021-08-12 18:09:41 +0300

    Deleted unused variable

--- KP7/matrix.c
+++ KP7/matrix.c
@@ -6,14 +6,13 @@
 
 void find_size_matrix(FILE* f, int* n, int* m)
 {
-    int e = 0, elems = 0, tmp = 0;
+    int e = 0, elems = 0;
 
     while (!feof(f)) {
-        e = tmp;
-        tmp = 0;
+        e = 0;
         if (fgetc(f) == '\n')
             *n += 1;
-        if (fscanf(f, "%d", &tmp))
+        if (fscanf(f, "%d", &e))
         {
             elems +=1; // elems - общее количество чисел в файле
             //printf("(%d,%d,%d,%d) ", e, tmp, elems, *n);
@@ -23,7 +22,7 @@ void find_size_matrix(FILE* f, int* n, int* m)
         }
     }
 
-    if (*n == 0 || elems == 1 || elems % *n != 0 || tmp != 0) {
+    if (*n == 0 || elems == 1 || elems % *n != 0 || e != 0) {
         printf("Error: The size of the matrix is incorrect or\n  there is no new line at the end of the file\n");
         exit(1);
     }
