be9eee6823e1b79e9382d074038d1eb20474956c
e-voloshinskaya <evgeniya@LAPTOP-0GMO7GOG.localdomain>
2021-08-12 17:10:29 +0300

    Completed file check

--- KP7/matrix.c
+++ KP7/matrix.c
@@ -14,18 +14,18 @@ void find_size_matrix(FILE* f, int* n, int* m)
         if (fscanf(f, "%d", &e))
         {
             elems +=1; // elems - общее количество чисел в файле
-            printf("(%d,%d) ", e, elems);
+            printf("(%d,%d,%d) ", e, elems, *n);
         } else {
             printf("Error: Input data should contain only numbers\n");
             exit(1);
         }
     }
 
-    if (elems % *n != 0) {
+    if (*n == 0 || elems == 1 || elems % *n != 0) {
         printf("Error: The size of the matrix is incorrect or\n  there is no new line at the end of the file\n");
         exit(1);
     }
-    
+
     *m = elems / *n;
     fseek(f, 0, SEEK_SET);
 // seek_set - начало файла, 0 - сдвиг (на 0 байтов)
